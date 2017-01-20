9de0c6e2afe0eba0729c235e564817ea76b3cc97
MatiTe11 <m.tegowski@wp.pl>
2017-01-21 00:53:11 +0100

    start

---
+++ Wanna/Bath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace Bath
+{
+    class Bath
+    {
+        Vector2 position;
+        double angle;
+        Texture2D seaTex;
+
+        public Bath()
+        {
+
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ Wanna/SeaScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Bath
+{
+    class SeaScreen
+    {
+        public SeaScreen()
+        {
+
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
